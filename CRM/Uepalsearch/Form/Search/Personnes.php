192b758b6e042a9a4be26f97e8d85bdc270c5874
Alain Benbassat <alain@businessandcode.eu>
2021-04-08 18:04:29 +0200

    first

---
+++ CRM/Uepalsearch/Form/Search/Personnes.php
@@ -0,0 +1,200 @@
+<?php
+use CRM_Uepalsearch_ExtensionUtil as E;
+
+class CRM_Uepalsearch_Form_Search_Personnes extends CRM_Contact_Form_Search_Custom_Base implements CRM_Contact_Form_Search_Interface {
+  public function buildForm(&$form) {
+    CRM_Utils_System::setTitle('Rechercher les personnnes reliés à une inspection et/ou un consistoire et/ou une paroisse');
+
+    $fields = [];
+    $fields[] = $this->addFieldRelations($form);
+    $fields[] = $this->addFieldInspections($form);
+    $fields[] = $this->addFieldConsistoires($form);
+    $fields[] = $this->addFieldParoisses($form);
+
+
+    $form->assign('elements', $fields);
+  }
+
+  public function &columns() {
+    $columns = [
+      'Identifiant' => 'contact_id',
+      'Prénom' => 'first_name',
+      'Nom' => 'last_name',
+      'Adresse e-mail' => 'email',
+    ];
+    return $columns;
+  }
+
+  public function all($offset = 0, $rowcount = 0, $sort = NULL, $includeContactIDs = FALSE, $justIDs = FALSE) {
+    $sql = $this->sql($this->select(), $offset, $rowcount, $sort, $includeContactIDs, NULL);
+    //var_dump($sql);exit;
+    return $sql;
+  }
+
+  public function select() {
+    $selectColumns = "
+      contact_a.id as contact_id,
+      contact_a.first_name,
+      contact_a.last_name,
+      e.email
+    ";
+
+    return $selectColumns;
+  }
+
+  public function from() {
+    $fromClause = "
+      FROM
+        civicrm_contact contact_a
+      LEFT OUTER JOIN
+        civicrm_email e ON e.contact_id = contact_a.id AND e.is_primary = 1
+    ";
+
+    return $fromClause;
+  }
+
+  public function where($includeContactIDs = FALSE) {
+    $params = [];
+
+    $orgContactsFilter = $this->getInspectionConsistoireParoisseFilter();
+    $relationshipFilter = $this->getRelationshipFilter();
+
+    $where = "
+        contact_a.is_deleted = 0
+      and
+        contact_a.contact_type = 'Individual'
+      and
+        exists (
+          select
+            r.id
+          from
+            civicrm_relationship r
+           where
+             r.contact_id_a = contact_a.id
+           and
+             r.is_active = 1
+           $orgContactsFilter
+           $relationshipFilter
+        )
+    ";
+
+    return $this->whereClause($where, $params);
+  }
+
+  private function getInspectionConsistoireParoisseFilter() {
+
+  }
+
+  private function getRelationshipFilter() {
+    $where = '';
+
+    $filter = CRM_Utils_Array::value('filter_relations', $this->_formValues);
+    if ($filter) {
+      $where = " and r.relationship_type_id in(" . implode(',', $filter) . ')';
+    }
+
+    return $where;
+  }
+
+  private function addFieldInspections(&$form) {
+    $fieldName = 'filter_inspection_consistoire_reforme';
+    $fieldLabel = 'Inspection';
+
+    $contactList = $this->getContactsOfType('inspection_consistoire_reforme');
+    $form->addElement('select', $fieldName, $fieldLabel, $contactList);
+
+    return $fieldName;
+  }
+
+  private function addFieldConsistoires(&$form) {
+    $fieldName = 'filter_consistoire_lutherien';
+    $fieldLabel = 'Consistoire luthérien';
+
+    $contactList = $this->getContactsOfType('consistoire_lutherien');
+    $form->addElement('select', $fieldName, $fieldLabel, $contactList);
+
+    return $fieldName;
+  }
+
+  private function addFieldParoisses(&$form) {
+    $fieldName = 'filter_paroisse';
+    $fieldLabel = 'Paroisse';
+
+    $contactList = $this->getContactsOfType('paroisse');
+    $form->addElement('select', $fieldName, $fieldLabel, $contactList);
+
+    return $fieldName;
+  }
+
+  private function addFieldRelations(&$form) {
+    $fieldName = 'filter_relations';
+    $fieldLabel = 'Relations';
+
+    $contactList = $this->getRelationships();
+    $form->add('select', $fieldName, $fieldLabel, $contactList, TRUE, [
+      'class' => 'crm-select2 huge',
+      'multiple' => 'multiple',
+    ]);
+
+    return $fieldName;
+  }
+
+  private function getContactsOfType($subType) {
+    $orgs = [];
+
+    $sql = "
+      select
+        id,
+        organization_name
+      from
+        civicrm_contact
+      where
+        is_deleted = 0
+      and
+        contact_type = 'Organization'
+      and
+        contact_sub_type like '%$subType%'
+      order by
+        organization_name
+	  ";
+    $dao = CRM_Core_DAO::executeQuery($sql);
+
+    // add an empty item at the beginning
+    $orgs[''] = '';
+
+    while ($dao->fetch()) {
+      $orgs[$dao->id] = $dao->organization_name;
+    }
+
+    return $orgs;
+  }
+
+  private function getRelationships() {
+    $rels = [];
+
+    $sql = "
+      select
+        id
+        , label_a_b
+      from
+        civicrm_relationship_type crt
+      where
+        contact_type_a  = 'Individual'
+      and
+        contact_type_b = 'Organization'
+      and
+        is_active = 1
+      and
+        id >= 11
+	  ";
+    $dao = CRM_Core_DAO::executeQuery($sql);
+
+    while ($dao->fetch()) {
+      $rels[$dao->id] = $dao->label_a_b;
+    }
+
+    return $rels;
+  }
+
+
+}
---
+++ uepalsearch.php
@@ -0,0 +1,172 @@
+<?php
+
+require_once 'uepalsearch.civix.php';
+// phpcs:disable
+use CRM_Uepalsearch_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function uepalsearch_civicrm_config(&$config) {
+  _uepalsearch_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function uepalsearch_civicrm_xmlMenu(&$files) {
+  _uepalsearch_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function uepalsearch_civicrm_install() {
+  _uepalsearch_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function uepalsearch_civicrm_postInstall() {
+  _uepalsearch_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function uepalsearch_civicrm_uninstall() {
+  _uepalsearch_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function uepalsearch_civicrm_enable() {
+  _uepalsearch_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function uepalsearch_civicrm_disable() {
+  _uepalsearch_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function uepalsearch_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _uepalsearch_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function uepalsearch_civicrm_managed(&$entities) {
+  _uepalsearch_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function uepalsearch_civicrm_caseTypes(&$caseTypes) {
+  _uepalsearch_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function uepalsearch_civicrm_angularModules(&$angularModules) {
+  _uepalsearch_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function uepalsearch_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _uepalsearch_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function uepalsearch_civicrm_entityTypes(&$entityTypes) {
+  _uepalsearch_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_thems().
+ */
+function uepalsearch_civicrm_themes(&$themes) {
+  _uepalsearch_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function uepalsearch_civicrm_preProcess($formName, &$form) {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function uepalsearch_civicrm_navigationMenu(&$menu) {
+//  _uepalsearch_civix_insert_navigation_menu($menu, 'Mailings', array(
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ));
+//  _uepalsearch_civix_navigationMenu($menu);
+//}
